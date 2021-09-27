df39ff90e3ed8ff11eb0600cab77703b17c17cd0
Sharma <sharmaac@wwt.com>
2021-09-27 10:00:40 +0530

    Added calculator implementation

---
+++ calculator.py
@@ -0,0 +1,19 @@
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def subtract(self):
+        return self.a - self.b
+
+    def multiply(self):
+        return self.a * self.b
+
+    def divide(self):
+        # TODO:- Add your code in the below function
+        # check for divide by zero error, using assert statements
+        # implement the division operation
+        return
